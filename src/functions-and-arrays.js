b82cbbb97e816b166fdb3d4ddd93767cf3888baf
Dev deRev <devderev@free.fr>
2022-02-22 15:07:26 +0100

    second commit : 22 passed

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -49,7 +49,13 @@ function sumNumbers(array) {
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(array) {
+  let sum = 0;
+  for(let i=0; i<array.length; i++){
+sum += array[i];
+  }
+  return sum;
+}
 
 
 
